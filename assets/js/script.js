ee6ef2bad193c7e079f346342345ad4af2709bb1
PRONOB BISWAS <80696443+pronobiswas@users.noreply.github.com>
2025-04-24 18:53:15 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,46 @@ let applicantInfo = {
     current_emloyer_name:"",
     current_emloyer_address:"",
     why_work_auth:"",
+    // phase6
+    full_name_of_prior_spouse : "",
+    date_marrige_began : "",
+    date_marrige_ended : "",
+    place_marrige_ended : "",
+    description_of_why_marrige_ended : "",
+    // phase7
+    name_of_child:"",
+    child_citizenship:"",
+    child_current_address:"",
+    child_immigration_status:"",
+    child_alien_registration_number:"",
+    child_birth_date:"",
+    child_country_of_birth:"",
+    child_average_earnings:"",
+    // phase8
+    street_and_number:"",
+    resided_from:"",
+    resided_to:"",
+    //phase9
+    full_name_of_employer:"",
+    full_address_of_employer:"",
+    earning_per_week:"",
+    type_of_work_preformed:"",
+    start_of_employment_last_10_years:"",
+    end_of_employment_last_10_years:"",
+    // phase10 
+    father_name:"",
+    father_cityzen_of_what_country:"",
+    father_relationship_to_me:"",
+    father_imigration_status:"",
+    father_alien_registration_number:"",
+    father_birth_date:"",
+    father_birth_country:"",
+    mother_cityzen_of_what_country:"",
+    mother_relationship_to_me:"",
+    mother_imigration_status:"",
+    mother_alien_registration_number:"",
+    mother_birth_date:"",
+    mother_birth_country:"",
 }
 let applicationInfoWarnning = {
     full_name:"",
@@ -147,7 +187,6 @@ document.getElementById('have_you_ever_left_the_US').addEventListener('input',(e
         ReasonforLeavingTable.classList.add('hidden');
     }
 })
-
 // -----validate phase two-----
 function validatePhaseTwo (){
     phase2Inputs.forEach((input)=>{
@@ -285,6 +324,7 @@ document.querySelector('#phase4 textarea').addEventListener('input',(e)=>{
         ...applicantInfo,
         [e.target.name]: e.target.value.trim(),
     }
+    document.querySelector('#phase4 textarea').classList.remove('warnInput')
 })
 // ----validate phase4 four-------
 function validatePhase4 (){
@@ -391,44 +431,244 @@ function handlePhaseFiveBtn(){
 }
 // %%%%%%%%%%%%%%phase five end here%%%%%%%%%%%%%%
 // $$$$$$$$$ PHASE 6 START HERE $$$$$$$$$$$$
+let isPhase6Validate = "false";
 const phase6 = document.getElementById('phase6');
+const phase6inputs = document.querySelectorAll('#phase6 input');
 const phase6btn = document.getElementById('phase6btn');
+
+phase6inputs.forEach((input) => {
+    input.addEventListener("click", () => {
+        input.classList.remove('warnInput');
+    });
+    input.addEventListener("input", (e) => {
+        applicantInfo={
+            ...applicantInfo,
+            [e.target.name]: e.target.value.trim(),
+        }
+    });
+});
 
 function handleAddmore1(){
     document.querySelector('#phase6 .part2').classList.toggle('hidden');
 }
+// ======== validate phase6 ==========
+function validatePhase6(){
+    phase6inputs.forEach((input)=>{
+        if(input.value == ""){
+            input.classList.add('warnInput');
+        }else{input.classList.remove('warnInput');}
+    });
+    if( !applicantInfo.full_name_of_prior_spouse || !applicantInfo.date_marrige_began || !applicantInfo.date_marrige_ended || !applicantInfo.place_marrige_ended || !applicantInfo.description_of_why_marrige_ended){
+        isPhase6Validate = "false"
+    }else{
+        isPhase6Validate = "true"
+    }
+}
+// ====phase 6 next btn ============
 phase6btn.addEventListener('click',()=>{
-    phase6.classList.add('hidden');
-    document.getElementById('phase7').classList.remove('hidden');
+    validatePhase6();
+    // console.log(isPhase6Validate);
+    // console.log(applicantInfo.full_name_of_prior_spouse ,applicantInfo.date_marrige_began ,applicantInfo.date_marrige_ended ,applicantInfo.place_marrige_ended ,applicantInfo.description_of_why_marrige_ended);
+    
+    if(isPhase6Validate == "true"){
+        phase6.classList.add('hidden');
+        document.getElementById('phase7').classList.remove('hidden');
+    }
 })
 // $$$$$$$$$ PHASE 6 end HERE $$$$$$$$$$$$
+
 // %%%%%%%%%%%% PHASE7 START HERE %%%%%%%%%%%%%
+
+let isValidatePhase7 = "false";
+const phase7 = document.getElementById('phase7');
+const phase7inputs = document.querySelectorAll('#phase7 input');
+phase7inputs.forEach((input) => {
+    input.addEventListener("click", () => {
+        input.classList.remove('warnInput');
+    });
+    input.addEventListener("input", (e) => {
+        applicantInfo={
+            ...applicantInfo,
+            [e.target.name]: e.target.value.trim(),
+        }
+    });
+});
+// ======== validate phase7 ==========
+function validatePhase7(){
+    phase7inputs.forEach((input)=>{
+        if(input.value == ""){
+            input.classList.add('warnInput');
+        }else{input.classList.remove('warnInput');}
+    });
+
+    if( !applicantInfo.name_of_child || !applicantInfo.child_citizenship || !applicantInfo.child_current_address || !applicantInfo.child_immigration_status || !applicantInfo.child_alien_registration_number || !applicantInfo.child_birth_date || !applicantInfo.child_country_of_birth || !applicantInfo.child_average_earnings){
+        isValidatePhase7 = "false"
+    }else{
+        isValidatePhase7 = "true"
+    }
+}
+
+
 document.getElementById('pahase7btn').addEventListener('click',()=>{
-    document.getElementById('phase7').classList.add('hidden');
-    document.getElementById('phase8').classList.remove('hidden');
+    validatePhase7()
+    console.log(isValidatePhase7);
+    
+    if(isValidatePhase7 == "true"){
+        document.getElementById('phase7').classList.add('hidden');
+        document.getElementById('phase8').classList.remove('hidden');
+    }
 });
 // %%%%%%%%%%%% PHASE7 END HERE %%%%%%%%%%%%%
-// %%%%%%%%%%%% PHASE8 START HERE %%%%%%%%%%%%%
-document.getElementById('pahase8btn').addEventListener('click',()=>{
-    document.getElementById('phase8').classList.add('hidden');
-    document.getElementById('phase9').classList.remove('hidden');
+
+// ############ PHASE8 START HERE #############
+let isValidatePhase8 = "false";
+const phase8 = document.getElementById('phase8');
+const phase8inputs = document.querySelectorAll('#phase8 input');
+phase8inputs.forEach((input) => {
+    input.addEventListener("click", () => {
+        input.classList.remove('warnInput');
+    });
+    input.addEventListener("input", (e) => {
+        applicantInfo={
+            ...applicantInfo,
+            [e.target.name]: e.target.value.trim(),
+        }
+    });
 });
-// %%%%%%%%%%%% PHASE8 END HERE %%%%%%%%%%%%%
+// ======== validate phase8 ==========
+function validatePhase8(){
+    phase8inputs.forEach((input)=>{
+        if(input.value == ""){
+            input.classList.add('warnInput');
+        }else{input.classList.remove('warnInput');}
+    });
+
+    if( !applicantInfo.street_and_number || !applicantInfo.resided_from || !applicantInfo.resided_to){
+        isValidatePhase8 = "false"
+    }else{
+        isValidatePhase8 = "true"
+    }
+}
+document.getElementById('pahase8btn').addEventListener('click',()=>{
+    validatePhase8();
+    console.log(isValidatePhase8);
+    if(isValidatePhase8 == "true"){
+        document.getElementById('phase8').classList.add('hidden');
+        document.getElementById('phase9').classList.remove('hidden');
+    }
+});
+// ############ PHASE8 END HERE ############
+
 // $$$$$$$$$$$ PHASE9 START HERE $$$$$$$$$$$$$$ 
+let isValidatePhase9 = "false";
+const phase9 = document.getElementById('phase9');
+const phase9inputs = document.querySelectorAll('#phase9 input');
+
+phase9inputs.forEach((input) => {
+    input.addEventListener("click", () => {
+        input.classList.remove('warnInput');
+    });
+    input.addEventListener("input", (e) => {
+        applicantInfo={
+            ...applicantInfo,
+            [e.target.name]: e.target.value.trim(),
+        }
+    });
+});
+
+function validatePhase9(){
+    phase9inputs.forEach((input)=>{
+        if(input.value == ""){
+            input.classList.add('warnInput');
+        }else{input.classList.remove('warnInput');}
+    });
+
+    if( !applicantInfo.full_name_of_employer || !applicantInfo.full_address_of_employer || !applicantInfo.earning_per_week || !applicantInfo.type_of_work_preformed || !applicantInfo.start_of_employment_last_10_years || !applicantInfo.end_of_employment_last_10_years){
+        isValidatePhase9 = "false"
+    }else{
+        isValidatePhase9 = "true"
+    }
+}
+
 document.getElementById('pahase9btn').addEventListener('click',()=>{
-    document.getElementById('phase9').classList.add('hidden');
-    document.getElementById('phase10').classList.remove('hidden');
+    console.log("phase9",isValidatePhase9);
+    console.log(applicantInfo.full_name_of_employer ,applicantInfo.full_address_of_employer ,applicantInfo.earning_per_week ,applicantInfo.type_of_work_preformed ,applicantInfo.start_of_employment_last_10_years ,applicantInfo.end_of_employment_last_10_years);
+    
+    
+    validatePhase9()
+    if(isValidatePhase9 == "true"){
+        console.log("kaj korar kotha to");
+        
+        document.getElementById('phase9').classList.add('hidden');
+        document.getElementById('phase10').classList.remove('hidden');
+    }else{console.log("kaj korar kotha na");}
 });
 // $$$$$$$$$$$ PHASE9 end HERE $$$$$$$$$$$$$$ 
 
+// =========phase 10 start here =============
+let isValidatePhase10 = "false";
+const phase10 = document.getElementById('phase10');
+const phase10inputs = document.querySelectorAll('#phase10 input');
+phase10inputs.forEach((input) => {
+    input.addEventListener("click", () => {
+        input.classList.remove('warnInput');
+    });
+    input.addEventListener("input", (e) => {
+        applicantInfo={
+            ...applicantInfo,
+            [e.target.name]: e.target.value.trim(),
+        }
+    });
+});
+function validatePhase10(){
+    phase9inputs.forEach((input)=>{
+        if(input.value == ""){
+            input.classList.add('warnInput');
+        }else{input.classList.remove('warnInput');}
+    });
+}
 
-// ReasonforLeavingTable
+// =========phase 10 end here =============
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// $$$$$$$$$ handle hidden Toggole $$$$$$$$$$
+function handleHiddenToogle(item){
+    item.classList.toggle('hidden')
+}
 
 // @@@@@@@@@@@@@@ handle previous button @@@@@@@@@@@@@ 
 function handlePreviousBtn(previousSection,currentSection){
-    console.log("previousSection:" ,previousSection);
-    console.log("currentSection:" ,currentSection);
     previousSection.classList.remove("hidden");
     currentSection.classList.add("hidden");
-    
 }
+// &&&&&&&&&&&&&&&& Handle Submit &&&&&&&&&&&&&&&
+// &&&&&&&&&&&&&&&& Handle Submit &&&&&&&&&&&&&&&
+// &&&&&&&&&&&&&&&& Handle Submit &&&&&&&&&&&&&&&
+let allsubmitvalue ={}
+let allInput=document.querySelectorAll('input');
+let allselect=document.querySelectorAll('select');
+let allInputValue = {};
+allInput.forEach((input)=>{
+    input.addEventListener('input',(e)=>{
+        allInputValue={
+            ...allInputValue,
+            [e.target.name]: e.target.value.trim()
+        }
+    });
+})
+allselect.forEach((select)=>{
+    select.addEventListener('change',(e)=>{
+        allInputValue={
+            ...allInputValue,
+            [e.target.name]: e.target.value.trim()
+        }
+    });
+})
+function handleSubmit (){
+    validatePhase9()
+    console.log("submit is clicked");
+    console.log(allInputValue);
+}
+// &&&&&&&&&&&&&&&& Handle Submit &&&&&&&&&&&&&&&
+// &&&&&&&&&&&&&&&& Handle Submit &&&&&&&&&&&&&&&
+// &&&&&&&&&&&&&&&& Handle Submit &&&&&&&&&&&&&&&
